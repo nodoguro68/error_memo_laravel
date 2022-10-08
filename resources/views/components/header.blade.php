1aea7ffea7027eb65e851e4611e29b98860fac16
kawada68 <nodoguro68@gmail.com>
2022-10-09 01:35:56 +0900

    headerのテンプレート作成

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<header class="l-header">
+    <div class="l-header__inner">
+        <h1 class="l-header__logo">
+            error memo
+        </h1>
+        <nav class="l-header__nav">
+            <ul class="l-header__nav-menu">
+                <li class="l-header__nav-item">
+                    <a href="" class="l-header__nav-link">ログイン</a>
+                </li>
+                <li class="l-header__nav-item">
+                    <a href="" class="l-header__nav-link">新規登録</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
